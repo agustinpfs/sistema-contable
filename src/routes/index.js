112d786c74961708973248b1e1040b7150b27f30
agustinpfs <agustinpfs@gmail.com>
2019-10-16 08:38:13 -0300

    clean code routes/index.js file and change variable name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,95 +1,49 @@
 const express = require('express');
 const router = express.Router(); 
 const Count = require('../model/counts'); 
-const Asiento = require('../model/asientos'); 
-// const viewAsiento = require('../views/crear-asiento'); 
-
-
-
+const Asiento = require('../model/asientos');
 
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-// router.get('/addCount', (req, res) => {
-//     res.render('addCount');
-// });
 
 router.get('/addCount', async (req, res) => {
     const counts = await Count.find(); 
-    // console.log(counts);
-    res.render('addCount', {    //en settings está configurado en app.js
-        counts   // lo mismo que countss:countss  // paso las tareas para mostrarse en pantalla
+    res.render('addCount', {
+        counts
     });
 });
 
 router.post('/addCount', async (req, res, next) => {
     const count = new Count(req.body);
-    // const asiento = new Asiento(req.body);
-    // //console.log(counts);
-    await count.save(); //para que se almacene en la db con async await 
-    // await asiento.save(); //para que se almacene en la db con async await 
-    res.redirect('/addCount');  // redirecciona a la ruta inicial del servidor
+    await count.save();
+    res.redirect('/addCount');
 });
 
+
 router.get('/crear-asiento', async (req, res) => {
-    const asientos = await Count.find();//.populate("name");
-    const nvo_asiento = await Asiento.find();//.populate("name");
-    // console.log(counts);
-    res.render('crear-asiento', {    //en settings está configurado en app.js
-        asientos, nvo_asiento   // lo mismo que countss:countss  // paso las tareas para mostrarse en pantalla
+    const asientos = await Count.find();
+    const nvo_asiento = await Asiento.find();
+    res.render('crear-asiento', {   
+        asientos, nvo_asiento 
     });
-    // res.render('crear-asiento', {    //en settings está configurado en app.js
-    //     nvo_asiento   // lo mismo que countss:countss  // paso las tareas para mostrarse en pantalla
-    // });
 });
 
 router.post('/crear-asiento', async (req, res, next) => {
     const nvo_asiento = new Asiento();
     nvo_asiento.fecha = req.body.fecha;
-    nvo_asient = req.body.cuenta;
-    nvo_asiento.cuenta = JSON.parse(nvo_asient);
-
-    // cu = JSON.parse(req.body.nvo_asient.cuenta)
-    // nvo_asiento.cuenta = JSON.parse(req.body.cuenta);
-    // const { cuenta } = req.body; 
-    // await nvo_asiento.set({ cuenta:allInfo });
-    // const asiento = new Asiento(req.body);
-    // //console.log(counts);
-    await nvo_asiento.save(); //para que se almacene en la db con async await 
-    // await q.save(); //para que se almacene en la db con async await 
-    // await asiento.save(); //para que se almacene en la db con async await 
-    res.redirect('/crear-asiento');  // redirecciona a la ruta inicial del servidor
+    cuentaJSON = req.body.cuenta;
+    nvo_asiento.cuenta = JSON.parse(cuentaJSON);
+    await nvo_asiento.save();
+    res.redirect('/crear-asiento');
 });
-
-
-
-
-
-
-// app.get("/crear-asiento", function (req, res) {
-//     Asiento.find({}, function (err, crear-asiento) {
-//         Count.populate(crear-asiento, { path: "name" }, function (err, crear-asiento) {
-//             res.status(200).send(crear-asiento);
-//         });
-//     });
-// });
-
-
-
-
-
-
-
-
-
 
 
 router.get('/edit/:id', async (req, res, next) => {
     let { id } = req.params;
     const count = await Count.findById(id);
-    // res.redirect('/addCount');
     res.render('edit', { count });
 });
 
@@ -97,19 +51,14 @@ router.post('/edit/:id', async (req, res, next) => {
     const { id } = req.params;
     await Count.update({ _id: id }, req.body); 
     res.redirect('/addCount');
-    // res.render('addCount', { count });
 });
 
 
-router.get('/delete/:id', async (req, res, next) => { //:id(puede ser cualquier nombre) irá el nº de la tarea y lo recibo
-    // console.log(req.params.id); 
-    //res.send(recivido);  
-    // req.params 
+router.get('/delete/:id', async (req, res, next) => {
     const { id } = req.params;
-    await Count.remove({ _id: id }); //lo borra de la db
+    await Count.remove({ _id: id });
     res.redirect('/addCount')
 });
-
 
 
 
