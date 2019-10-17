32919bc068f8c35872e3635f68099f600c31394a
agustinpfs <agustinpfs@gmail.com>
2019-10-17 11:00:50 -0300

    add autoincremental engine

--- src/model/asientos.js
+++ src/model/asientos.js
@@ -1,43 +1,16 @@
 const mongoose = require('mongoose');
+var AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
-// var Cuenta = mongoose.model('counts');
-
-
-
-// const AsientoSchema = new Schema({
-//     numero_asiento: Number,
-//     fecha: Date,
-//     cuentas: [{
-//         nombre: String,
-//         debe: String,
-//         haber: String,
-//         importe: Number
-//     }],
-
-// const 
 
 
 
 const AsientoSchema = new Schema({
     numero_asiento: Number,
     fecha: Date,
-    // cuenta: JSON.parse({ type: String }),
     cuenta: Array,
-    // cuenta: [{
-    //             nombre: String,
-    //             debe: String,
-    //             haber: String,
-    //             importe: Number
-    //         }],
-    // dh: String,
-    // importe: Number
-    // name: { type: Schema.ObjectId, ref: "Cuenta" }  
-    // type: String,
-    // type: [{ type: Schema.Types.ObjectId, ref: 'counts' }]
 })
 
+AsientoSchema.plugin(AutoIncrement, { id: 'order_seq', inc_field: 'numero_asiento' });
 
-// const Up = mongoose.model('asientos', AsientoSchema);
-// const user = await Up.findOne({ name: 'leo' });
 
 module.exports = mongoose.model('asientos', AsientoSchema);
