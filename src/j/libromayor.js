2363f11f9db3c1fec9e9baca7cff3850ebe94532
agustinpfs <agustinpfs@gmail.com>
2019-10-22 20:48:25 -0300

    add saldo libro mayor

--- src/j/libromayor.js
+++ src/j/libromayor.js
@@ -5,3 +5,15 @@ document.querySelectorAll('#nomLMyr > option').forEach((option) => {
     if (options.includes(option.value)) option.remove()
     else options.push(option.value)
 })
+
+// function saldoFunction() {
+//     saldo = 0;
+//     for (let i = 0; i < cuenta.length; i++) {
+//         if (cuenta[i].debe) {
+//             sumD = sumD + cuenta[i].importe;
+//         }
+//         if (cuenta[i].haber) {
+//             sumH = sumH + cuenta[i].importe;
+//         }
+        
+//     }
