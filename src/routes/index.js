be677fbc5599fd0967899f373716c64d9f9f4689
agustinpfs <agustinpfs@gmail.com>
2019-10-21 21:40:08 -0300

    filter for libro mayor

--- src/routes/index.js
+++ src/routes/index.js
@@ -68,6 +68,16 @@ router.get('/libro-mayor', async (req, res, next) => {
     });
 });
 
+router.get('/libro-mayo/:name', async (req, res, next) => {
+    const { name } = req.params;
+    const lmayor = await Asiento.find({"cuenta.nombre":name});
+    // res.redirect('/libro-mayo')
+
+    res.render('libro-mayo', {
+        lmayor
+    })
+});
+
 
 
 module.exports = router;
